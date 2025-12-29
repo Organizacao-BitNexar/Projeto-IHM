a21b899acc4f0d48ab0c2500f9364d97a791fb42
Joao Vitor <joao.neves@aluno.ifsertao-pe.edu.br>
2025-12-29 20:38:20 -0300

    models sistema solar

---
+++ core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class CorpoCeleste(models.Model):
+    CATEGORIAS = (
+        ('star', 'Estrela'),
+        ('planet', 'Planeta'),
+        ('moon', 'Lua'),
+    )
+
+    nome = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=10, choices=CATEGORIAS)
+    tipo = models.CharField(max_length=100, blank=True)
+    distancia = models.CharField(max_length=100, blank=True)
+    diametro = models.CharField(max_length=100, blank=True)
+    massa = models.CharField(max_length=100, blank=True)
+    temperatura = models.CharField(max_length=100, blank=True)
+    composicao = models.TextField(blank=True)
+    curiosidades = models.TextField()
+
+    planeta_pai = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='luas'
+    )
+
+    def __str__(self):
+        return self.nome
+# Create your models here.
